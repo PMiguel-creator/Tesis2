187e6184fa1636321482b03c446096f971e62d29
Jose Miguel <jeganag@example.com>
2026-03-19 23:42:55 -0300

    configurar variables de entorno

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+
+const firebaseConfig = {
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const firestoreDatabaseId = process.env.VITE_FIREBASE_FIRESTORE_DATABASE_ID;
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app, firestoreDatabaseId);
 export const auth = getAuth(app);
 
 // Validate Connection to Firestore
